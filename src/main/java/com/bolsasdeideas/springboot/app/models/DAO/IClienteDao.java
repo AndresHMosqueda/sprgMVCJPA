d4c227f5a023c8dfbd2d0f1ce9d110fd9170d4bd
AndresHMosqueda <andreshm94@gmail.com>
2019-10-01 22:08:08 -0500

    Pagination

--- src/main/java/com/bolsasdeideas/springboot/app/models/DAO/IClienteDao.java
+++ src/main/java/com/bolsasdeideas/springboot/app/models/DAO/IClienteDao.java
@@ -3,13 +3,17 @@ package com.bolsasdeideas.springboot.app.models.DAO;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.bolsasdeideas.springboot.app.models.entity.Cliente;
 
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
 
 	//Cliente es la clase entity que esta mapeada a la tabla
+	
+	
+	
 	
 	
 	
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
-INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Hdz', 'andres@test.com', '2019-09-21');
-INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(2, 'Sam', 'sAS', 'sam@test.com', '2019-09-26');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Hdz', 'andres@test.com', '2019-09-22');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(2, 'Sam', 'SAS', 'sam@test.com', '2019-09-26');
+
