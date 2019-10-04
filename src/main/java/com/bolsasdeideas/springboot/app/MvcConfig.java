d599e4c7f75c8ee1c286c9165597d38013c17d3e
AndresHMosqueda <andreshm94@gmail.com>
2019-10-03 20:43:50 -0500

    Done

--- src/main/java/com/bolsasdeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsasdeideas/springboot/app/MvcConfig.java
@@ -14,7 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	
-	@Override
+	/*@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
@@ -26,7 +26,7 @@ public class MvcConfig implements WebMvcConfigurer {
 		log.info(resourcePath);
 		registry.addResourceHandler("/uploads/**")
 		.addResourceLocations(resourcePath);
-	}
+	}*/
 
 	
 }  
